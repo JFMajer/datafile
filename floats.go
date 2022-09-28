805f43453fe8930c0267904cea017dd596f105af
JFMajer <jakubmajer@live.com>
2022-09-28 21:15:47 +0200

    changed function to use slice instead of array

--- floats.go
+++ floats.go
@@ -8,8 +8,8 @@ import (
 )
 
 // GetFloats reads a float64 from each of a provided file.
-func GetFloats(fileName string) ([4]float64, error) {
-	var numbers [4]float64
+func GetFloats(fileName string) ([]float64, error) {
+	var numbers []float64
 	file, err := os.Open(fileName)
 	if err != nil {
 		return numbers, err
@@ -17,11 +17,11 @@ func GetFloats(fileName string) ([4]float64, error) {
 	i := 0
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		numbers[i], err = strconv.ParseFloat(scanner.Text(), 64)
+		number, err := strconv.ParseFloat(scanner.Text(), 64)
 		if err != nil {
 			return numbers, err
 		}
-		i++
+		numbers = append(numbers, number)
 	}
 	err = file.Close()
 	if err != nil {
